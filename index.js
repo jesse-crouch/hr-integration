705d455b68877f02a01c1093b5a519f0a8ec26dd
Jesse <jesse@pop-os.localdomain>
2022-02-15 16:31:05 -0500

    Test creating envelope

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const request = require('request');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -8,7 +9,22 @@ app.use(bodyParser.json());
 const port = 3044;
 
 app.post('/newHire', (req, res) => {
-    console.log(req.body.key);
+    console.log(req.body);
+
+    // Create envelope
+    var options = {
+        uri: 'https://' + '/restapi/v2.1/accounts/{accountId}/envelopes',
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }
+    }
+    request(options, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(result);
+        }
+    });
+
     res.sendStatus(200);
 });
 
